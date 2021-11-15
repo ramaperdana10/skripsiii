2a87690f3de3b3bb05cd1b9f856c9f9030a89403
Kirima-Kun <74716247+ramaperdana10@users.noreply.github.com>
2021-11-15 10:44:48 +0700

    Add files via upload

---
+++ tools/main.py
@@ -0,0 +1,22 @@
+from sys import exit
+from textwrap import dedent
+from modules.parser import *
+from modules.utils import *
+from modules.downloader import *
+from modules.show import *
+from modules.csv_downloader import *
+from modules.bounding_boxes import *
+from modules.image_level import *
+
+
+ROOT_DIR = ''
+DEFAULT_OID_DIR = os.path.join(ROOT_DIR, 'prepare_dataset')
+
+if __name__ == '__main__':
+
+    args = parser_arguments()
+
+    if args.command == 'downloader_ill':
+        image_level(args, DEFAULT_OID_DIR)
+    else:
+        bounding_boxes_images(args, DEFAULT_OID_DIR)
